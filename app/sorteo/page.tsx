c2d48f4c7b0218ebdf6b2713c3316750bcdba4c8
laurahora463 <lauraahora4632025@gmail.com>
2025-08-17 16:47:10 -0300

    nuevo numero

--- app/sorteo/page.tsx
+++ app/sorteo/page.tsx
@@ -10,8 +10,8 @@ export default function SorteoPage() {
   // --- Variables para los puestos manuales (NO son estados de UI) ---
   // Define aquí los números que quieres para el primer y segundo puesto.
   // Si no quieres un puesto manual, déjalo como 'undefined' o 'null'.
-  const primerPuestoDefinido: number | undefined  = undefined; // Ejemplo: El número 1010 será el primer ganador
-  const segundoPuestoDefinido: number | undefined = undefined; // Ejemplo: El número 2020 será el segundo ganador
+  const primerPuestoDefinido: number | undefined  = 65612; // Ejemplo: El número 1010 será el primer ganador
+  const segundoPuestoDefinido: number | undefined = 27048; // Ejemplo: El número 2020 será el segundo ganador
   // Si no quieres segundo puesto manual:
   // const segundoPuestoDefinido: number | undefined = undefined; 
   // ------------------------------------------------------------------
